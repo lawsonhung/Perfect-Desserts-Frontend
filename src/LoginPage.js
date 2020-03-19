3fc2f76e135ed9d342f900e4a2b05b4690835326
lawsonhung <lawsonhung@yahoo.com>
2020-03-18 20:46:41 -0400

    added localStorage

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -9,13 +9,34 @@ export default class LoginPage extends Component {
   handleChange = (e) => {
     this.setState({[e.target.name]: e.target.value })
   }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    })
+    .then(res => res.json())
+    .then(data => {
+      // Checking what data is returned
+      // console.log(data)
+
+      if (data.token){
+        localStorage.token = data.token;
+      }
+    })
+  }
   
   render() {
     return (
       <div>
         {/* {<button>Switch to Signup</button>} */}
         <h1>Log in please!</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input onChange={this.handleChange} value={this.state.username} type="text" name="username" />
           <input onChange={this.handleChange} value={this.state.password} type="password" name="password" />
           <input type="submit" value="Log in" />

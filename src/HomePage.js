a9983b1c37e391933cb84c71acbca1f5694498dc
lawsonhung <lawsonhung@yahoo.com>
2020-05-20 17:41:13 -0400

    Add button on homepage to log in

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 
 export default class HomePage extends Component {
+
+  handleClick = (e) => {
+    e.preventDefault();
+    this.props.history.push('/login');
+  }
+
   render () {
     console.log(this.props);
 
     return (
       <div>
         Hi, I'm the HomePage!
+        <button onClick={this.handleClick}>Log in here!</button>
       </div>
     );
   }
